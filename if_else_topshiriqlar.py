cfde3e94a73ec929a2a5f62f7098d50a38e55ce4
shahzodbekavazxonov <154684331+shahzodbekavazxonov@users.noreply.github.com>
2024-12-16 19:42:20 +0500

    If_else topshiriqlari(1-qism)

---
+++ if_else_topshiriqlar.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 16 16:01:08 2024
+
+@author: Shahzod
+"""
+
+#cars=['toyota','mazda','hyundai','gm','kia','bmw']
+#for n in cars:
+ #   if n=="bmw" or n=="gm":
+  #      print(n.upper())
+   # else:
+    #    print(n)
+    
+    
+
+
+#cars=['toyota','mazda','hyundai','gm','kia','bmw']
+#for n in cars:
+#   if n!="bmw" and n!="gm":
+#      print(n)
+#   else:
+#      print(n.upper())
+
+
+
+#ism=input("Ismingizni kiriting: ")
+#if ism.lower()=="admin":
+#    print("Xush kelibsiz,Admin\n Foydalanuvchilar ro'yxatini ko'rasizmi?")
+#else:
+#    print(f"Xush kelibsiz,{ism.title()}")
+
+
+#a=int(input("A son kiriting: "))
+#b=int(input("B son kiriting: "))
+#if a==b:
+#    print("Bu ikkala son bir-biriga teng ekan")
+#else:
+#    print("Bu ikkala son bir-biriga teng emas")
+
+
+#son=int(input("Istalgan son kiriting: "))
+#if son>=0:
+#    print(f"Ushbu {son} soni musbat")
+#else:
+#    print(f"Ushbu {son} soni manfiy")
+
+
+#a=int(input("Son kiriting: "))
+#if a>=0:
+#    print(int(pow(a,1/2)))
+#else:
+#    print("Musbat son kiriting")
+
+
+#a=int(input("Son kiriting: "))
+#if a%2==0:
+#    print(f"Ushbu {a} soni juft")
+#else:
+#    print(f"Ushbu {a} soni toq")
+
+
